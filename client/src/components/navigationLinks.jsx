621869c61be164ef996e19d6718f29528b07320c
Dominic Fraser <mrdjfraser@gmail.com>
2017-07-08 21:16:13 +0100

    fixed webpack issue due to Link not being link in import

--- client/src/components/navigationLinks.jsx
+++ client/src/components/navigationLinks.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import AppBar from 'react-toolbox/lib/app_bar'
 import Navigation from 'react-toolbox/lib/navigation'
-import Link from 'react-toolbox/lib/Link'
+import Link from 'react-toolbox/lib/link'
 import Drawer from 'react-toolbox/lib/drawer'
 import ApiCommunicatorHelper from '../helpers/apiCommunicatorHelper'
 
